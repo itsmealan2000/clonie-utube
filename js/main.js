345756d8b771a13025510e75d46b437fe7d5641f
Alan v John <itsmealan2000@gmail.com>
2023-11-13 09:31:55 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var button = document.querySelector('[data-bs-target="#collapseWidthExample"]');
+    var contentSection = document.getElementById('content');
+
+    button.addEventListener('click', function () {
+      contentSection.classList.toggle('content-shifted');
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function () {
+    var button = document.querySelector('[data-bs-target="#collapseWidthExample"]');
+    var contentSection = document.getElementById('recent');
+
+    button.addEventListener('click', function () {
+      contentSection.classList.toggle('content-shifted');
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function () {
+    var button = document.querySelector('[data-bs-target="#collapseWidthExample"]');
+    var contentSection = document.getElementById('container-vid');
+
+    button.addEventListener('click', function () {
+      contentSection.classList.toggle('content-shifted');
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function () {
+    var button = document.querySelector('[data-bs-target="#collapseWidthExample"]');
+    var contentSection = document.getElementById('newvid');
+
+    button.addEventListener('click', function () {
+      contentSection.classList.toggle('content-shifted');
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var button = document.querySelector('[data-bs-target="#collapseWidthExample"]');
+    var contentSection = document.getElementById('container-newvid');
+
+    button.addEventListener('click', function () {
+      contentSection.classList.toggle('content-shifted');
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    var button = document.querySelector('[data-bs-target="#collapseWidthExample"]');
+    var contentSection = document.getElementById('shorts');
+
+    button.addEventListener('click', function () {
+      contentSection.classList.toggle('content-shifted');
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function () {
+    var button = document.querySelector('[data-bs-target="#collapseWidthExample"]');
+    var contentSection = document.getElementById('container-shorts');
+
+    button.addEventListener('click', function () {
+      contentSection.classList.toggle('content-shifted');
+    });
+  });
